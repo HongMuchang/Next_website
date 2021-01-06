c8b2624793f5abb41395858ca55bdafdb81e2c0a
HongMuchan <muchang0824@gmail.com>
2021-01-06 11:36:39 +0900

    [new]Next-Website

---
+++ pages/blog-page.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const Blog = () => {
+
+  return(
+  <Layout title="Blog">  
+  <p>Blog Page</p>
+    </Layout>
+  )
+}
+
+export default Blog;
---
+++ pages/contact-page.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const Contact = () => {
+  return(
+
+    <Layout title="Contact">  
+    <p>Contact Page</p>
+    </Layout>
+  ) 
+}
+
+export default Contact;
